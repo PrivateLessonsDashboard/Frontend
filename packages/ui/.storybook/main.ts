c01281e509dec112133a120230623b337ef4b2d1
Velo ⚡ <snajper695yt@gmail.com>
2024-03-01 22:10:53 +0100

    feat(turborepo): added themes plugin for storybook

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -6,6 +6,7 @@ import { join, dirname } from "path";
 function getAbsolutePath(value: string): any {
   return dirname(require.resolve(join(value, "package.json")));
 }
+
 const config: StorybookConfig = {
   stories: [
     "../src/components/**/*.mdx",
@@ -16,6 +17,7 @@ const config: StorybookConfig = {
     getAbsolutePath("@storybook/addon-essentials"),
     getAbsolutePath("@storybook/addon-onboarding"),
     getAbsolutePath("@storybook/addon-interactions"),
+    getAbsolutePath("@storybook/addon-themes"),
   ],
   framework: {
     name: getAbsolutePath("@storybook/react-webpack5"),
@@ -36,4 +38,5 @@ const config: StorybookConfig = {
     return config;
   },
 };
+
 export default config;
--- packages/ui/.storybook/preview.ts
+++ packages/ui/.storybook/preview.ts
@@ -1,4 +1,5 @@
-import type { Preview } from "@storybook/react";
+import { withThemeByDataAttribute } from "@storybook/addon-themes";
+import type { Preview, ReactRenderer } from "@storybook/react";
 import "../src/styles/globals.css";
 
 const preview: Preview = {
@@ -11,6 +12,16 @@ const preview: Preview = {
       },
     },
   },
+  decorators: [
+    withThemeByDataAttribute<ReactRenderer>({
+      themes: {
+        light: "light",
+        dark: "dark",
+      },
+      defaultTheme: "light",
+      attributeName: "class",
+    }),
+  ],
 };
 
 export default preview;
