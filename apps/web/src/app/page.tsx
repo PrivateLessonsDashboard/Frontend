0a52c92f7af57d132658dcfc591fcfc9d4ebb977
Velo ⚡ <snajper695yt@gmail.com>
2024-03-07 22:08:11 +0100

    fix: remove ui package, and change eslint configuration

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,10 +1,7 @@
-import { Button } from "@repo/ui";
-
 function HomePage() {
   return (
     <section className="bg-gray-400 w-full h-screen">
       <h1>Home Page</h1>
-      <Button>Example button from UI</Button>
     </section>
   );
 }
